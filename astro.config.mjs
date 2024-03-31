50980b880d33a09e6b4c7f18eea3dbee8d451350
steven nevins <steven.nevins.js@gmail.com>
2024-03-30 21:54:39 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import alpine from "@astrojs/alpinejs";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [alpine(), tailwind()],
+});
